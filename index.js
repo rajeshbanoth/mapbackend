32b1e5172900022a0daedb7b795fe2f2b73c1653
rajeshbanoth <banothrajesh97@gmail.com>
2022-11-22 14:55:57 +0530

    first commit

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get('/',(req,res)=>{
+    res.send("server running")
+})
+
 
 const server = app.listen(process.env.PORT||9000,()=>{
     console.log("server running on "+process.env.PORT)
